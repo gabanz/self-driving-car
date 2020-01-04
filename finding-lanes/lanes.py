729a4cba29fbac481862b3cc2c6d5b6025306729
Faiz Azhar <faiz@cloudflare.com>
2020-01-05 02:51:32 +0800

    Optimizing 2

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -36,8 +36,7 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
@@ -59,6 +58,6 @@ lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineL
 averaged_lines = average_slope_intercept(lane_image, lines)
 line_image = display_lines(lane_image, averaged_lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow('result', line_image)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
 
