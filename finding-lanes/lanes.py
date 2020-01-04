a509f004bf22ebef61fbc3bfeb65a382f7a5e417
Faiz Azhar <faiz@cloudflare.com>
2020-01-05 01:59:59 +0800

    Line detection - Hough transform

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -7,6 +7,12 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            print(line)
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([
@@ -21,6 +27,8 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
-cv2.imshow('result', cropped_image )
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+cv2.imshow('result', cropped_image)
 cv2.waitKey(0)
 
